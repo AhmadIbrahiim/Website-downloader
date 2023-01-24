d572d2a85a230f834d3039d96036576cc0f1a3f1
ajbutt1 <56531100+ajbutt1@users.noreply.github.com>
2023-01-24 11:30:36 +0500

    Update index.js

--- wget/index.js
+++ wget/index.js
@@ -11,7 +11,7 @@ module.exports=(io,data)=>{
  * --no-parent http://example.org
  * --mirror – Makes (among other things) the download recursive.
  * --convert-links – convert all the links (also to stuff like CSS stylesheets) to relative, so it will be suitable for offline viewing.
- * --adjust-extension – Adds suitable extensions to filenames (html or css) depending on their content-type.
+// * --adjust-extension – Adds suitable extensions to filenames (html or css) depending on their content-type.
  * --page-requisites – Download things like CSS style-sheets and images required to properly display the page offline.
  * --no-parent – When recurring do not ascend to the parent directory. It useful for restricting the download to only a portion of the site.
  */
